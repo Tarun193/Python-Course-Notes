b4eead973775efb0439db57e35e9275c75e02e1f
Tarun <tarunchawla.7463@gmail.com>
2022-06-03 12:47:38 +0530

    conditional statements

---
+++ conditional statements/1.py
@@ -0,0 +1,12 @@
+age  = int(input('enter you age:'))
+
+if age >= 18:
+    print("you can vote")
+
+elif age >= 30:
+    print("you will get free meal")
+
+else:
+    print("No you can't vote")
+
+print("bye")
